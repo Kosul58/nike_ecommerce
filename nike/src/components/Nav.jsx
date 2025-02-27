4335312071920b4235b0d6b2d17c01c6bf0e4d37
Kosul58 <kosulgrg@gmail.com>
2025-02-27 17:16:19 +0545

    b

--- nike/src/components/Nav.jsx
+++ nike/src/components/Nav.jsx
@@ -5,17 +5,24 @@ import { navLinks } from "../constanst";
 const Nav = () => {
   return (
     <header className="padding-x py-8 fixed z-20 w-full">
-      <nav className="flex justify-center items-center max-container bg-blue-200 w-[80%] h-[60px] rounded-xl sm:w-[90%]">
-        <a href="/" className="lg:ml-20">
+      <nav className="flex justify-evenly items-center max-container bg-blue-200 w-[85%] h-[60px] rounded-xl sm:w-[95%]">
+        <a
+          href="/"
+          className="lg:ml-20 max-[1200px]:ml-[-10]
+        "
+        >
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden mr-[-10%]">
+        <ul className="flex-1 flex justify-center items-center gap-2 max-lg:hidden mr-[-10%]">
           {navLinks.map((item) => {
             return (
-              <li key={item.label}>
+              <li
+                key={item.label}
+                className="w-[15%] h-[50px] flex justify-center items-center"
+              >
                 <a
                   href={item.href}
-                  className="fonts-montserrat leading-normal text-lg text-black font-montserrat hover:text-[25px] hover:text-coral-red"
+                  className="fonts-montserrat leading-normal text-lg text-black font-montserrat hover:text-[150%] hover:text-coral-red "
                 >
                   {item.label}
                 </a>
@@ -23,7 +30,7 @@ const Nav = () => {
             );
           })}
         </ul>
-        <div>
+        <div className="absolute right-5">
           <img
             src={hamburger}
             alt="Hamburger"
