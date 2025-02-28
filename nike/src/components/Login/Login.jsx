6165b0abe8036cd751a6ce2603115878acb31271
Kosul58 <kosulgrg@gmail.com>
2025-02-28 16:30:57 +0545

    f

--- nike/src/components/Login/Login.jsx
+++ nike/src/components/Login/Login.jsx
@@ -1,14 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [login, setLogin] = useState(false);
+
+  const toggleModal = (e) => {
+    e.preventDefault(); // Prevent the default form submission behavior
+    setLogin(!login); // Toggle login/signup state
+  };
+
   return (
-    <section className="flex flex-col bg-cyan-300 w-full h-screen sm:min-h-[450px]  max-sm:h-[120lvh] ">
-      <div
-        className="absolute top-28 left-1/2 transform -translate-x-1/2 w-[90%] max-sm:w-[95%] h-[75lvh] max-sm:top-20 max-sm:h-[100lvh]
-      bg-slate-400 rounded-lg shadow-xl flex justify-center items-center"
-      >
-        <div className="w-[50%] h-[100%] bg-coral-red rounded-s-lg "></div>
-        <div className="w-[50%] h-[100%] bg-yellow-200 rounded-e-lg "></div>
+    <section className="flex flex-col bg-cyan-300 w-full h-screen">
+      <div className="absolute top-[15%] left-1/2 transform -translate-x-1/2 w-[100%] h-auto bg-inherit rounded-lg flex flex-1 justify-center items-center flex-wrap">
+        {/* Login Form */}
+        <form
+          className="w-[45vw] h-[75vh] bg-coral-red min-w-[600px] xl:rounded-s-xl max-lg:rounded-t-lg flex flex-col justify-center items-center gap-2 max-[700px]:min-w-[400px] max-[450px]:min-w-[300px]"
+          onSubmit={toggleModal} // Handles form submit
+        >
+          <div
+            className={`w-[100%] h-[35%] flex justify-center items-center flex-col gap-2 ${
+              login ? "hidden" : "flex"
+            }`}
+          >
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-24 h-10 bg-white rounded-lg hover:bg-green-400 hover:scale-110"
+          >
+            Login
+          </button>
+        </form>
+
+        {/* Signup Form */}
+        <form
+          className="w-[45vw] h-[75vh] bg-yellow-200 min-w-[600px] xl:rounded-e-xl max-lg:rounded-b-lg flex flex-col justify-center items-center gap-2 max-[700px]:min-w-[400px] max-[450px]:min-w-[320px]"
+          onSubmit={toggleModal} // Handles form submit
+        >
+          <div
+            className={`w-[100%] h-[60%] flex justify-center items-center flex-col gap-2 ${
+              login ? "flex" : "hidden"
+            }`}
+          >
+            <input
+              type="text"
+              placeholder="Username"
+              className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+            />
+            <input
+              type="email"
+              placeholder="kosul@nike.com"
+              className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-24 h-10 bg-white rounded-lg hover:bg-green-400 hover:scale-110"
+          >
+            Signup
+          </button>
+        </form>
       </div>
     </section>
   );
--- nike/src/components/Nav.jsx
+++ nike/src/components/Nav.jsx
@@ -28,14 +28,14 @@ const Nav = () => {
 
   return (
     <header className="padding-x py-8 fixed z-20 w-full">
-      <nav className="flex justify-center items-center max-container bg-blue-200 w-[85%] h-[60px] rounded-xl sm:w-[95%] shadow-[0_1px_5px_rgba(0,0,0,0.5)] max-sm:mt-[-1rem]">
+      <nav className="flex justify-center items-center max-container bg-blue-200 w-[85%] h-[60px] rounded-xl sm:w-[95%] shadow-[0_1px_5px_rgba(0,0,0,0.5)] max-sm:mt-[-1.5rem] mt-[-1rem]">
         <Link to="/" className="ml-10 min-sm:ml-0">
           <img src={headerLogo} alt="Logo" width={130} height={29} />
         </Link>
         <ul className="flex-1 flex justify-center items-center gap-2 max-lg:hidden mr-[-5%]">
           {navLinks.map((item) => (
             <li
-              key={item.label}
+              // key={item.label}
               className="w-[15%] h-[50px] flex justify-center items-center"
             >
               {item.link ? (
@@ -60,7 +60,7 @@ const Nav = () => {
         <Link to="/cart">
           <FaShoppingCart
             size={26}
-            className="mr-10 cursor-pointer hover:scale-110 hover:text-white hidden min-[1030px]:block"
+            className="mr-10 cursor-pointer hover:scale-110 hover:text-coral-red hidden min-[1030px]:block"
           />
         </Link>
 
@@ -80,7 +80,7 @@ const Nav = () => {
         </div>
         <div
           ref={modalref}
-          className={`absolute top-28 w-[80%] h-[70vh] border-2 z-30 rounded-xl bg-white shadow-[0_0_20px_rgba(0,0,0,0.5)] transition-opacity duration-200 ${
+          className={`absolute top-20 w-[80%] h-[70vh] min-h-[300px] border-2 z-30 rounded-xl bg-white shadow-[0_0_20px_rgba(0,0,0,0.5)] transition-opacity duration-200 ${
             showModal ? "opacity-100" : "opacity-0 pointer-events-none"
           } flex items-center justify-center flex-col lg:hidden `}
         >
@@ -89,16 +89,16 @@ const Nav = () => {
             className="hover:text-red-500 absolute right-0 top-0"
             onClick={toggleModal}
           />
-          <ul className="flex-1 flex justify-center items-center gap-4 flex-col">
+          <ul className="flex-1 flex justify-evenly items-center flex-col">
             {navLinks.map((item) => (
               <li
                 key={item.label}
-                className="w-[200px] h-[50px] flex justify-center items-center rounded-sm cursor-pointer  sm:h-[25px]"
+                className="min-w-[200px] min-h-[30px] w-auto h-auto flex justify-center items-center rounded-sm cursor-pointer"
               >
                 {item.link ? (
                   <Link
                     to={item.link}
-                    className="font-montserrat text-lg text-black hover:text-[110%] hover:text-coral-red"
+                    className="font-montserrat text-lg text-black hover:text-[110%] hover:text-coral-red text-[20px] sm:text-[30px] lg:text-[40px]"
                     onClick={toggleModal}
                   >
                     {item.label}
@@ -110,7 +110,7 @@ const Nav = () => {
                       !isHome && handleHrefClick(item.href);
                       toggleModal;
                     }}
-                    className="font-montserrat text-lg text-black hover:text-[110%] hover:text-coral-red"
+                    className="font-montserrat text-lg text-black hover:text-[110%] hover:text-coral-red text-[20px] sm:text-[30px] lg:text-[40px]"
                   >
                     {item.label}
                   </a>
@@ -120,7 +120,7 @@ const Nav = () => {
             <Link to="/cart">
               <FaShoppingCart
                 size={26}
-                className="cursor-pointer hover:scale-110 hover:text-white"
+                className="cursor-pointer hover:scale-110 hover:text-coral-red"
               />
             </Link>
           </ul>
