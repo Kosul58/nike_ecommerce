c8ffef4b92a9e01af69847a173fcdf3bdc2f82ab
Kosul58 <kosulgrg@gmail.com>
2025-02-28 17:03:52 +0545

    h

--- nike/src/components/Login/Login.jsx
+++ nike/src/components/Login/Login.jsx
@@ -1,17 +1,44 @@
 import React, { useRef, useState } from "react";
 
 const Login = () => {
-  const [login, setLogin] = useState(false);
+  const [login, setLogin] = useState(true);
+  const [signup, setSignup] = useState(true);
+  const [logged, setLogged] = useState(false);
   const luname = useRef(null);
   const lpwd = useRef(null);
+  const suname = useRef(null);
+  const spwd = useRef(null);
+  const scpwd = useRef(null);
+  const semail = useRef(null);
 
   const handleLogin = (e) => {
     e.preventDefault();
-    alert(luname.current.value, lpwd.current.value);
+    const x = luname.current.value;
+    const y = lpwd.current.value;
+    if (login) {
+      if (!x || !y) {
+        alert("Enter both password and value");
+      } else {
+        alert("Login Successfull");
+      }
+    } else {
+      setLogin(!login);
+      setSignup(!signup);
+    }
   };
-  const toggleModal = (e) => {
+
+  const handleSignup = (e) => {
     e.preventDefault();
-    setLogin(!login); // Toggle login/signup state
+    const x = suname.current.value;
+    const y = spwd.current.value;
+    const y2 = scpwd.current.value;
+    const z = semail.current.value;
+    if (signup) {
+      setSignup(!signup);
+      setLogin(!login);
+    } else {
+      alert("Login Successfull");
+    }
   };
 
   return (
@@ -26,7 +53,7 @@ const Login = () => {
           >
             <div
               className={`w-[100%] h-[35%] flex justify-center items-center flex-col gap-2 ${
-                login ? "hidden" : "flex"
+                login ? "flex" : "hidden"
               }`}
             >
               <input
@@ -53,32 +80,36 @@ const Login = () => {
           {/* Signup Form */}
           <form
             className="w-[45vw] h-[75vh] bg-yellow-200 min-w-[600px] xl:rounded-e-xl max-lg:rounded-b-lg flex flex-col justify-center items-center gap-2 max-[700px]:min-w-[400px] max-[450px]:min-w-[320px]"
-            onSubmit={toggleModal}
+            onSubmit={handleSignup}
           >
             <div
               className={`w-[100%] h-[60%] flex justify-center items-center flex-col gap-2 ${
-                login ? "flex" : "hidden"
+                signup ? "hidden" : "flex"
               }`}
             >
               <input
                 type="text"
                 placeholder="Username"
                 className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+                ref={suname}
               />
               <input
                 type="email"
                 placeholder="kosul@nike.com"
                 className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+                ref={spwd}
               />
               <input
                 type="password"
                 placeholder="Password"
                 className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+                ref={scpwd}
               />
               <input
                 type="password"
                 placeholder="Confirm Password"
                 className="w-[60%] h-[50px] px-3 rounded-lg max-[700px]:min-w-[80%]"
+                ref={semail}
               />
             </div>
             <button
